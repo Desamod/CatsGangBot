8fd467d99721bbb84b6797778425038060dfcfc0
Andrey Koptug <16992157+Desamod@users.noreply.github.com>
2024-10-05 00:00:37 +0300

    added new youtube tasks

--- bot/config/youtube_tasks.py
+++ bot/config/youtube_tasks.py
@@ -2,6 +2,9 @@ YOUTUBE_TASKS: dict[str, str] = {
     "Boost Your Cats Earnings!": 'dildo',
     "Futures Trading PART 1": 'AIRNODE',
     "Futures Trading PART 2": 'WEI',
-    "FREE Crypto": 'dip'
+    "FREE Crypto": 'dip',
+    "How To Make 10x On Crypto": 'ABSTRACT',
+    "How To Make 10x On Crypto PART 2": 'AUCTION',
+    "How To Make 10x On Crypto PART 3": 'AUDIT'
 }
 
